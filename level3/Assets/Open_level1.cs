34b3731dd636a7ea55d62bc6a0c0963b3b2272bb
Omar1Sorour <omarmas0510@gmail.com>
2025-12-15 19:24:10 +0200

    Cutscenes

---
+++ level3/Assets/Open_level1.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Open_level1 : MonoBehaviour
+{
+    public void onClickStart(){
+        SceneManager.LoadScene(3);
+    }
+}
